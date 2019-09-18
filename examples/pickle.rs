ee056d5f71a17a02fc3eeaf05770702c8c421be0
Georg Brandl <georg@python.org>
2019-09-18 17:46:58 +0200

    example: return Result from main() instead of using unwrap

--- examples/pickle.rs
+++ examples/pickle.rs
@@ -11,6 +11,7 @@ extern crate serde_pickle;
 extern crate serde_json;
 
 use std::env;
+use std::error::Error;
 use std::fs::File;
 use std::io::{stdin, stdout, Read};
 use std::process::exit;
@@ -18,7 +19,7 @@ use std::process::exit;
 use serde_json as json;
 use serde_pickle as pickle;
 
-fn main() {
+fn main() -> Result<(), Box<dyn Error>> {
     let args = env::args().collect::<Vec<_>>();
     if args.len() < 2 {
         println!("Usage: pickle (decode | transcode | to_json | from_json) [filename]");
@@ -30,31 +31,34 @@ fn main() {
         println!("from_json: encode pickle from json");
         exit(1);
     }
-    let reader: Box<Read> = if args.len() == 3 {
-        Box::new(File::open(&args[2]).unwrap())
+
+    let reader: Box<dyn Read> = if args.len() == 3 {
+        Box::new(File::open(&args[2])?)
     } else {
         Box::new(stdin())
     };
+
     match &*args[1] {
         "decode" => {
-            let decoded: pickle::Value = pickle::value_from_reader(reader).unwrap();
+            let decoded: pickle::Value = pickle::value_from_reader(reader)?;
             println!("{:#?}", decoded);
         },
         "transcode" => {
-            let decoded: pickle::Value = pickle::value_from_reader(reader).unwrap();
-            pickle::value_to_writer(&mut stdout(), &decoded, true).unwrap();
+            let decoded: pickle::Value = pickle::value_from_reader(reader)?;
+            pickle::value_to_writer(&mut stdout(), &decoded, true)?;
         },
         "to_json" => {
-            let decoded: json::Value = pickle::from_reader(reader).unwrap();
+            let decoded: json::Value = pickle::from_reader(reader)?;
             println!("{:#?}", decoded);
         },
         "from_json" => {
-            let decoded: json::Value = json::from_reader(reader).unwrap();
-            pickle::to_writer(&mut stdout(), &decoded, false).unwrap();
+            let decoded: json::Value = json::from_reader(reader)?;
+            pickle::to_writer(&mut stdout(), &decoded, false)?;
         }
         _ => {
             println!("No such subcommand.");
             exit(1);
         }
     }
+    Ok(())
 }
