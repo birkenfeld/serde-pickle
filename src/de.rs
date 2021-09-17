746e6b57b140535212df5ce78a84486bf1bbf375
Jeroen Bakker <j.bakker@atmind.nl>
2021-09-17 20:11:06 +0200

    Moved documentation from lib header to de.
    Solves one issue of the code review.
    The docs is put in the header of de.rs as it fits better than
    `(value_)from_reader`.

--- src/de.rs
+++ src/de.rs
@@ -9,6 +9,51 @@
 //! Note: Serde's interface doesn't support all of Python's primitive types.  In
 //! order to deserialize a pickle stream to `value::Value`, use the
 //! `value_from_*` functions exported here, not the generic `from_*` functions.
+//!
+//! # Streaming multiple objects
+//!
+//! By default `(value_)from_reader` closes the input stream. It is possible
+//! to deserialize multiple pickle objects from a single stream by
+//! implementing a custom reader.
+//!
+//! ```
+//! use std::io::Read;
+//! use serde_pickle::{Deserializer, Result, DeOptions};
+//! use serde::Deserialize;
+//!
+//! struct PickleReader<R>
+//! where
+//!     R: Read + Sized,
+//! {
+//!     de: Deserializer<R>,
+//! }
+//!
+//! impl<R: Read + Sized> PickleReader<R>
+//! {
+//!    fn new(reader: R) -> PickleReader<R> {
+//!        PickleReader {
+//!            de: Deserializer::new(reader, DeOptions::new()),
+//!        }
+//!    }
+//!
+//!    pub fn read_object<'de, T: Deserialize<'de>>(&mut self) -> Result<T> {
+//!        let value = Deserialize::deserialize(&mut self.de)?;
+//!        Ok(value)
+//!    }
+//! }
+//!
+//! let input = [0x80, 0x04, 0x95, 0x09, 0x00, 0x00, 0x00, 0x00,
+//!              0x00, 0x00, 0x00, 0x8c, 0x05, 0x48, 0x65, 0x6c,
+//!              0x6c, 0x6f, 0x94, 0x2e, 0x80, 0x04, 0x95, 0x0a,
+//!              0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x8c,
+//!              0x06, 0x70, 0x69, 0x63, 0x6b, 0x6c, 0x65, 0x94,
+//!              0x2e, 0x00];
+//! let mut reader = PickleReader::new(std::io::Cursor::new(input));
+//! let string1: String = reader.read_object().unwrap();
+//! let string2: String = reader.read_object().unwrap();
+//! assert_eq!(&string1, "Hello");
+//! assert_eq!(&string2, "pickle");
+//! ```
 
 use std::io;
 use std::mem;
